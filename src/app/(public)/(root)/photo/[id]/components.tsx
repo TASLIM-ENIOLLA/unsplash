3962319ebd0c81c47890865da03d35b4adf7e41e
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2025-03-08 09:17:50 +0100

    PhotoDisplayCard finished

--- src/app/(public)/(root)/photo/[id]/components.tsx
+++ src/app/(public)/(root)/photo/[id]/components.tsx
@@ -1,41 +1,78 @@
 "use client";
 
-import { ChevronLeft } from "lucide-react";
+import { X, ChevronLeft, ChevronRight } from "lucide-react"
 
-export function PhotoDisplayCard() {
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { useEscapeKey } from "@/hooks/use-escape-key";
+
+import { PhotoDisplayCardProps } from "./types";
+
+export function PhotoDisplayCard({ id, urls, user, width, height, description }: PhotoDisplayCardProps) {
+  const router = useRouter();
+  
+  function closeDisplayCard() {
+    router.back();
+  }
+
+  useEscapeKey(function () {
+    closeDisplayCard();
+  });
+  
   return (
-    <figure className="flex h-full gap-10 items-center">
+    <figure className="flex gap-10 h-full items-center">
       <div className="flex-none">
-        <button type="button" className="block size-12 rounded-full bg-foreground">
-          <ChevronLeft size="20" className="mx-auto text-background" />
+        <button type="button" className="size-12 block rounded-full bg-foreground text-background">
+          <ChevronLeft size="30" className="mx-auto" />
         </button>
       </div>
-      <div className="flex-1 border">
-        <div className="my-10 bg-foreground rounded-lg overflow-hidden">
-          <div className="flex flex-col">
-            <div className="flex-1">
-              <div className="h-96" />
-            </div>
-            <div className="flex-none">
-              <div className="p-5 text-background">
-                <h4 className="text-lg font-bold truncate">
-                  <span className="capitalize">
-                    taslim musa-azeez
-                  </span>
-                </h4>
-                <p className="truncate">
-                  <span className="capitalize">
-                    nigeria
-                  </span>
-                </p>
+      <div className="flex-1 max-h-full">
+        <div className="flex flex-col justify-center">
+          <div className="flex-none">
+            <figure className="flex flex-col rounded-lg overflow-hidden bg-foreground">
+              <div className="flex-none">
+                <div className="z-0 relative">
+                  <Image
+                    width={width}
+                    height={height}
+                    src={urls.regular}
+                    alt={String(description)}
+                  />
+                  <div className="top-0 left-0 absolute">
+                    <div className="p-5">
+                      <button
+                        type="button"
+                        className="size-8 rounded-full cursor-pointer bg-foreground"
+                        onClick={closeDisplayCard}
+                      >
+                        <X size="20" className="mx-auto text-background" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+              <div className="flex-none">
+                <div className="p-5 text-background">
+                  <h4 className="text-lg font-bold truncate">
+                    <span className="capitalize">
+                      {user.first_name} {user.last_name}
+                    </span>
+                  </h4>
+                  <p className="truncate">
+                    <span className="capitalize">
+                      {user.location || "location N/A"}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </figure>
           </div>
         </div>
       </div>
       <div className="flex-none">
-        <button type="button" className="block size-12 rounded-full bg-foreground">
-          <ChevronLeft size="20" className="mx-auto text-background" />
+        <button type="button" className="size-12 block rounded-full bg-foreground text-background">
+          <ChevronRight size="30" className="mx-auto" />
         </button>
       </div>
     </figure>
--- src/app/(public)/(root)/photo/[id]/layout.tsx
+++ src/app/(public)/(root)/photo/[id]/layout.tsx
@@ -2,9 +2,9 @@ import { LayoutProps } from "./types";
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <section className="z-10 top-0 left-0 fixed h-screen w-screen backdrop-blur-xs bg-background/70">
+    <section className="z-10 top-0 fixed left-0 w-full min-h-screen backdrop-blur-xs bg-background/70">
       <div className="h-full container">
-        <div className="h-full mx-auto max-w-xl">
+        <div className="h-full mx-auto max-w-2xl">
           {children}
         </div>
       </div>
