2a17122ac1aee72272b554505b500a6542f0c981
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2025-03-09 00:51:28 +0100

    Fade in and jump up animations added

--- src/app/(public)/(root)/layout.tsx
+++ src/app/(public)/(root)/layout.tsx
@@ -1,16 +1,19 @@
+"use client";
+
+import { motion } from "motion/react";
 import { Fragment, Suspense } from "react";
 
 import { LayoutProps } from "./types";
 
 export default function Layout({ searchBar, photoGrid, children, searchPhrase }: LayoutProps) {
   return (
-    <Fragment>
+    <motion.section initial={{ opacity: 0, translateY: "5%" }} animate={{ opacity: 1, translateY: 0, transition: { duration: 1 } }}>
       {children}
       <Suspense fallback={null}>
         {searchBar}
         {searchPhrase}
       </Suspense>
       {photoGrid}
-    </Fragment>
+    </motion.section>
   );
 }
