f1178ef58425bfa6b74266f8af7f1fcd73de9585
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2025-04-08 01:22:17 +0100

    Name branding on Search Bar

--- src/app/(public)/(root)/@searchBar/default.tsx
+++ src/app/(public)/(root)/@searchBar/default.tsx
@@ -4,8 +4,11 @@ export default function Page() {
   return (
     <section className="py-20">
       <div className="container">
-        <div className="mx-auto max-w-xl">
+        <div className="mx-auto max-w-xl space-y-1">
           <SearchBar />
+          <p className="text-right text-foreground">
+            Designed and developed by Taslim Eniolla.
+          </p>
         </div>
       </div>
     </section>
